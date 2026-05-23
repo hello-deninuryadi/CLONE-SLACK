999ef71d068c9c78c3fba1f0238ded9259daa30b
Deni Nuryadi <Deninuryadi63@gmail.com>
2026-05-23 14:37:13 +0700

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import exppress from "express";
+import {ENV} from "./config/env.js";
+
+
+const app = exppress();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello  World");
+});
+
+console.log("mongo.uri : " , ENV.MONGO_URI);
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+});
