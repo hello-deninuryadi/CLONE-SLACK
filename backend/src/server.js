5ce2f4d4188eebd7dc99a2806f91cee2847fccb1
Deni Nuryadi <Deninuryadi63@gmail.com>
2026-05-24 21:04:19 +0700

    Auth Implementaion (Clerk , Monggo DB and Inggest)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
-import exppress from "express";
+import express from "express";
 import {ENV} from "./config/env.js";
 import {connectDB} from "./config/db.js";
+import {clerkMiddleware} from "@clerk/express";
+import {inngest, functions} from "./config/inngest.js";
+import {serve} from "inngest/express";
+
 
 import dns from "node:dns/promises";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 
 
-const app = exppress();
+const app = express();
+
+
+//req.auth will be available in all routes after this middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 
 app.get("/", (req, res) => {
@@ -15,8 +28,20 @@ app.get("/", (req, res) => {
 });
 
 
+const startServer = async () => {
+    try{
+        await connectDB();
+        if (ENV.NODE_ENV !== "production") {
+            app.listen(ENV.PORT, () => {
+                console.log(`Server is running on port ${ENV.PORT}`);
+            });
+        }
+    }catch(err){
+        console.error("Error starting server:", err);
+        process.exit(1);
+    }
+}
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-    connectDB();
-});
+startServer();
+
+export default app;
